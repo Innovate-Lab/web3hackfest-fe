eb94a23f3ab68ea489248161a36144cb9aab5f18
quigiaosu103 <52100996@student.tdtu.edu.vn>
2024-12-17 16:18:28 +0700

    :rocket: feat: fix heading responsivce

--- src/layout/components/Heading/index.tsx
+++ src/layout/components/Heading/index.tsx
@@ -6,7 +6,7 @@ import {
 } from "@/provider/DataContext";
 import { title } from "process";
 import { useContext, useEffect, useState } from "react";
-import { AlignJustify, MoveRight } from "lucide-react";
+import { AlignJustify, MoveRight, X } from "lucide-react";
 import Button from "@/components/Button";
 const pages = [
   {
@@ -41,6 +41,7 @@ const pages = [
 
 function Heading() {
   const [logged, setLogged] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
   const { activePage, setActivePage } = useDataContext();
   useEffect(() => {
     setActivePage(parseInt(localStorage.getItem("activePage") || "0"));
@@ -59,7 +60,7 @@ function Heading() {
         />
       </div>
 
-      <div className="menu-wrapper sm:flex hidden">
+      <div className="menu-wrapper sm:flex hidden ">
         {pages.map((page, index) => (
           <HeadignButton
             key={index}
@@ -78,15 +79,58 @@ function Heading() {
         {logged ? (
           <div className="">
             {/* handle show option button or user avatar */}
-            <div className="sm:hidden block">
-              <AlignJustify stroke="#fff" />
+            <div className="sm:hidden block ">
+              <span
+                className="h-fit sm:hidden"
+                onClick={() => setShowMenu(!showMenu)}
+              >
+                <AlignJustify stroke="#fff" />
+              </span>
+              {showMenu && (
+                <div
+                  className={`menu-wrapper gap-6 sm:hidden absolute top-0 right-0 w-full h-[100vh] bg-black pt-6 pr-4 rounded-[10px] shadow-sm 
+                    ${
+                      showMenu
+                        ? "transition-all ease-linear duration-200 h-[100vh]"
+                        : "h-0"
+                    }
+                    `}
+                >
+                  <div className="w-full flex justify-end">
+                    <span
+                      className="p-2 cursor-pointer"
+                      onClick={() => {
+                        setShowMenu(false);
+                      }}
+                    >
+                      <X stroke="#fff" />
+                    </span>
+                  </div>
+
+                  <div className="p-4">
+                    {pages.map((page, index) => (
+                      <HeadignButton
+                        textLeft
+                        key={index}
+                        text={page.title}
+                        to={page.to}
+                        active={activePage === index}
+                        click={() => {
+                          handlePageChange(index);
+                          setShowMenu(false);
+                        }}
+                      />
+                    ))}
+                  </div>
+                </div>
+              )}
             </div>
             <div className="sm:flex hidden items-center gap-4 cursor-pointer">
               <span className="text-white text-[17px] font-[500]">
                 suistark02
               </span>
               <div
-                className="w-[44px] h-[44px] rounded-[50%] bg-cover bg-center  border-[1px] border-primary"
+                className="w-[44px] relative h-[44px] rounded-[50%] bg-cover bg-center  border-[1px] border-primary"
                 style={{
                   backgroundImage: `url('assets/images/persons/openart-image_Jyj2md-t_1726731886987_raw.jpg')`,
                 }}
