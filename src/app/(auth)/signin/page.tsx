a50a207939abd9b8f83f0f804e52678bfe6f52f8
quigiaosu103 <52100996@student.tdtu.edu.vn>
2024-12-20 16:08:23 +0700

    responsive: signin and signup

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,13 +1,16 @@
-
 import SigninForm from "@/components/signin/SigninForm";
 import React, { Suspense } from "react";
 
 const Page = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <div className="flex flex-col gap-8">
-        <div className="text-white text-[45px] text-center">Đăng nhập</div>
-        <SigninForm />
+      <div className="flex flex-col gap-4 sm:gap-8 sm:w-fit w-[92%]">
+        <div className="text-white text-[24px]  sm:text-[45px] text-center">
+          Đăng nhập
+        </div>
+        <div className="w-full sm:w-fit">
+          <SigninForm />
+        </div>
       </div>
     </Suspense>
   );
