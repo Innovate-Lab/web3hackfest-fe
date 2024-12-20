14029b2ca0149298dca26f9efb3df6ed8c0e3377
Loc <52100818@student.tdtu.edu.vn>
2024-12-20 12:24:07 +0700

    remove page in auth

--- src/app/(auth)/page.tsx
+++
@@ -1,9 +0,0 @@
-export const metadata = {
-  title: "Sign up",
-  description: "Root layout",
-};
-function RootLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
-
-export default RootLayout;
